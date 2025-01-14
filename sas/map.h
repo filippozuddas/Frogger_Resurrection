8fc9d6bf56bd51d5b839d2e0d00922c74a906dde
filippozuddas <filipposimonezuddas@gmail.com>
2025-01-14 17:47:21 +0100

    removed sas folder

--- sas/map.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void inizializza_mappa(); // Inizializza ncurses e i colori
-void disegna_mappa();     // Disegna la mappa del gioco
-void chiudi_mappa();      // Chiude ncurses e libera risorse
